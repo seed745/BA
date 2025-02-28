f184a57682aecfa8dd5cd886d1072a1f620874d8
seed745 <seed745@gmail.com>
2025-02-28 17:31:19 +0100

    better plot

--- source_compare.py
+++ source_compare.py
@@ -10,12 +10,12 @@ source_area = []
 geofacs = []
 for i in range(9):
     #reading the 0.hits file 
-    df = pd.read_csv("/home/jasper/OneDrive/Uni/Bachelor/sim/verif/eq10_source_"+str(i)+".0.hits",skiprows=1,names=["evid","detid"], sep='\s+')
+    df = pd.read_csv("/home/jasper/OneDrive/Uni/Bachelor/sim/source/eq10_source_"+str(i)+".0.hits",skiprows=1,names=["evid","detid"], sep='\s+')
 
     #reading and adding the other.hits to df
     erg =[]
     for j in [1,2]:
-        erg.append(pd.read_csv("/home/jasper/OneDrive/Uni/Bachelor/sim/verif/eq10_source_"+str(i)+"."+str(j)+".hits",skiprows=1,names=["evid","detid"], sep='\s+'))
+        erg.append(pd.read_csv("/home/jasper/OneDrive/Uni/Bachelor/sim/source/eq10_source_"+str(i)+"."+str(j)+".hits",skiprows=1,names=["evid","detid"], sep='\s+'))
         #print(erg[i-1].size)
         df = pd.concat([df,erg[j-1]],axis=0)
 
@@ -43,10 +43,10 @@ tabelle(np.round(source_area,2),np.round(geofacs,4))
 plt.plot(source_area,geofacs)
 #plt.xlim(left=2)
 plt.axhline(y=0.0268, linestyle ="--", linewidth= 2, color = "orange")
-plt.axvline(x=2.89, linestyle="--", linewidth= 2, color = "green")
+plt.axvline(x=2.13, linestyle="--", linewidth= 2, color = "green")
 plt.ylabel("geometry factor in 1/cm²sr")
 plt.xlabel("source area in cm²")
-plt.legend(["Simulation","GF after Sullivan","FOV projection"])
+plt.legend(["Simulation","Analytical","FOV projection"])
 plt.show()
 
     

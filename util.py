4a96ead1d291e05207d5fbc9486d3d40cba5b739
seed745 <seed745@gmail.com>
2025-03-04 17:15:25 +0100

    eval funk and tabular funk addded

--- util.py
+++ util.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 
 def print_sci_l(numbers, precision=3):
     for num in numbers:
@@ -7,12 +10,60 @@ def print_sci_l(numbers, precision=3):
 def print_sci_n(num, precision=3):
         print("{:.{}e}".format(num, precision))
 
-def tabelle(a,b): 
+def latex_float(num):
+    # Format the number as scientific notation with a precision of 5 decimal places
+    formatted = "{:.3e}".format(num)
+    
+    # Split the formatted string into base and exponent parts
+    base, exponent = formatted.split('e')
+    
+    # Handle the case where the base starts with '0.' by removing the leading "0."
+    if base[0] == '0' and base[1] == '.':
+        base = base[1:]  # Remove the leading '0.'
+    
+    # Construct the LaTeX scientific notation
+    latex_expr = f"{base} \\cdot 10^{{{int(exponent)}}}"
+    
+    return latex_expr
+
+
+
+def tabelle2(a,b): 
     for i in range(len(a)):
-        print(str(a[i])+" & "+str(b[i])+" \\" "\\")
+        print("$"+latex_float(a[i])+"$"+" & "+"$"+latex_float(b[i])+"$"+" \\" "\\")
+    print("Ende")
+
+def tabelle4(a,b,c,d): 
+    for i in range(len(a)):
+        print(a[i]+" & "+"$"+latex_float(b[i])+"$"+" & "+"$"+latex_float(c[i])+"$"+" & "+"$"+latex_float(d[i])+"$"+" \\" "\\")
     print("Ende")
 
 def tabelle_einfach(a): 
     for i in range(len(a)):
-        print(str(a[i][0])+" & "+str(a[i][1])+" & "+str(a[i][2])+" & "+str(a[i][3])+" & "+str(a[i][4])+" \\" "\\")
+        print(latex_float(a[i][0])+" & "+latex_float(a[i][1])+" & "+latex_float(a[i][2])+" & "+latex_float(a[i][3])+" & "+latex_float(a[i][4])+" \\" "\\")
     print("Ende")
+
+def eval(file, area):
+        #reading the 0.hits file 
+    df = pd.read_csv(file,skiprows=1,names=["evid","detid"], sep='\s+')
+
+    print(df.size)
+
+    #reading and adding the other.hits to df
+
+    for i in [1,2]:
+        temp = pd.read_csv(file[:-6]+str(i)+".hits",skiprows=1,names=["evid","detid"], sep='\s+')
+        print(temp.size)
+        df = pd.concat([df,temp],axis=0)
+
+    #counting the coincidence events
+    dupli = df[df.duplicated(subset=["evid"],keep=False)]
+    print(dupli.size)
+    group = dupli.groupby("detid").indices
+
+    hits = len(group[0])
+    ratio = hits/1000000
+    geofac = np.pi*area*ratio
+
+    return geofac
+

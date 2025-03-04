e96eb74c730dcf68d5944a363e16696a9fb6433a
seed745 <seed745@gmail.com>
2025-03-04 17:14:43 +0100

    small changes

--- eval_hits.py
+++ eval_hits.py
@@ -4,6 +4,7 @@ import pandas as pd
 import argparse
 import numpy as np
 
+
 #input of the filename filename.0.hits
 parser = argparse.ArgumentParser(prog="eval_hits", description="Used for analyzing .hits files form Geant4", epilog="Many Thanks to Nicolas Rohrbeck")
 parser.add_argument("file", type = str, help="inputfile of form path/to/filename.hits")
@@ -14,33 +15,37 @@ args = vars(parser.parse_args())
 #reading the 0.hits file 
 df = pd.read_csv(args["file"],skiprows=1,names=["evid","detid"], sep='\s+')
 
-#print(df.size)
+print(df.size)
 
 #reading and adding the other.hits to df
-erg =[]
-for i in [1,2,3]:
-    erg.append(pd.read_csv(args["file"][:-6]+str(i)+".hits",skiprows=1,names=["evid","detid"], sep='\s+'))
-    #print(erg[i-1].size)
-    df = pd.concat([df,erg[i-1]],axis=0)
+
+for i in [1,2]:
+    temp = pd.read_csv(args["file"][:-6]+str(i)+".hits",skiprows=1,names=["evid","detid"], sep='\s+')
+    #print(temp.size)
+    df = pd.concat([df,temp],axis=0)
 
 #counting the coincidence events
 dupli = df[df.duplicated(subset=["evid"],keep=False)]
+#print(dupli.head())
 group = dupli.groupby("detid").indices
 
 hits = len(group[0])
-print("Source area in cm²: ")
+ratio = hits/1000000
+np.pi*args["area"]*ratio
+
+
+
+""" print("Source area in cm²: ")
 print(args["area"])
 print()
 print("Coincidence hits:")
 print(hits)
 print()
 #print("Hit/Sim Ratio: ")
-ratio = hits/1000000
+
 #print(ratio)
 #print()
 
 
-
-
 print("Geometry factor in 1/sr*cm²: ")
-print(np.pi*args["area"]*ratio)
+print(np.pi*args["area"]*ratio) """

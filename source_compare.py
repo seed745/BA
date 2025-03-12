213a29795f25e4e152d31f00bbf7d2b67afefe47
seed745 <seed745@gmail.com>
2025-03-12 10:20:13 +0100

    updated to circle source

--- source_compare.py
+++ source_compare.py
@@ -3,9 +3,9 @@
 import pandas as pd 
 import numpy as np
 from matplotlib import pyplot as plt
-from util import tabelle
+from util import tabelle2
 
-source_length = [2,4,6,7,8,8.5,9,12,15] #in mm
+source_length = [2,4,6,8,9,9.5,10,12,14] #in mm
 source_area = []
 geofacs = []
 for i in range(9):
@@ -29,7 +29,7 @@ for i in range(9):
     ratio = hits/1000000
 
     #area in cm²
-    area = (source_length[i]*0.1*2)**2
+    area = (source_length[i]/10)**2*np.pi 
     source_area.append(area)
 
     #geometry factor in 1/cm²sr
@@ -37,10 +37,10 @@ for i in range(9):
     geofacs.append(geofac)
 
 #LATEX tabular
-tabelle(np.round(source_area,2),np.round(geofacs,4))
+tabelle2(np.round(source_area,2),np.round(geofacs,4))
 
 #plot generation
-plt.errorbar(source_area,geofacs,yerr= 0.0009,capsize = 5, capthick = 2)
+plt.errorbar(source_area,geofacs,yerr= 0.0004,capsize = 5, capthick = 2)
 #plt.xlim(left=2)
 plt.axhline(y=0.0268, linestyle ="--", linewidth= 2, color = "orange")
 plt.axvline(x=2.13, linestyle="--", linewidth= 2, color = "green")

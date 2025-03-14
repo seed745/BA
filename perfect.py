33b0dce7bf6bcafbaca8e557d2e259506eb2e38d
seed745 <seed745@gmail.com>
2025-03-14 17:34:49 +0100

    added multiple runs

--- perfect.py
+++ perfect.py
@@ -2,48 +2,67 @@ import numpy as np
 import util as u
 import pandas as pd
 
-r_source = 70 #mm
-area = (r_source/10)**2*np.pi
+run = []
+run.append(["/home/jasper/OneDrive/Uni/Bachelor/sim/perfect/fit.0.hits",10**6,70])
+run.append(["/home/jasper/OneDrive/Uni/Bachelor/sim/perfect/fit_10M.0.hits",10**7,70])
+run.append(["/home/jasper/OneDrive/Uni/Bachelor/sim/perfect/fov.0.hits",10**6,83])
 
-file = "/home/jasper/OneDrive/Uni/Bachelor/sim/perfect/fit.0.hits"
 
-df = pd.read_csv(file,skiprows=1,names=["evid","detid","x","y","z","dphi","dtheta"], sep='\s+')
 
-#print(df.head(5))
-#print(df.size)
+for i in range(len(run)):
 
-#reading and adding the other.hits to df
+    file , pn, r = run[i][0],run[i][1],run[i][2]  #pn = particle number, r = radius of source in mm
 
-for i in [1,2]:
-    temp = pd.read_csv(file[:-6]+str(i)+".hits",skiprows=1,names=["evid","detid","x","y","z","dphi","dtheta"], sep='\s+')
-    #print(temp.size)
-    df = pd.concat([df,temp],axis=0)
+    area = (r/10)**2*np.pi
 
-#counting the coincidence events
-dupli = df[df.duplicated(subset=["evid"],keep=False)]
-#print(dupli.head(10))
-group = dupli.groupby(by=["detid"]).indices
 
-hits = len(group[0])
-print(f"All hits: {hits}")
-ratio = hits/1000000
-geofac = np.pi*area*ratio
+    print(f"Run number: {i+1}")
+    print()
+    print(f"Number of Particles: ")
+    u.print_sci_n(pn)
+    print(f"Source area in cm²: ")
+    u.print_sci_n(area)
+    print()
 
-print(f"Full Geometry factor: {np.round(geofac,2)}")
-print()
 
-#GF of a cut out area of two 10x10 PDs as validation
-cut = df[df["x"].between(-5,5) & df["y"].between(-5,5)]
-dublicut = cut[cut.duplicated(subset=["evid"],keep=False)]
+    df = pd.read_csv(file,skiprows=1,names=["evid","detid","x","y","z","dphi","dtheta"], sep='\s+')
 
-print(f"cutsize: {cut.size}")
-print(dublicut.head(10))
+    #print(df.head(5))
+    #print(df.size)
 
-cutgroup = dublicut.groupby(by=["detid"]).indices
-print(cutgroup)
-hits = len(cutgroup[0])
-print(f"10x10 hits: {hits}")
-ratio = hits/1000000
-geofac = np.pi*area*ratio
+    #reading and adding the other.hits to df
 
-print(f"10x10 cut GF: {np.round(geofac,5)}")
+    for j in [1,2]:
+        temp = pd.read_csv(file[:-6]+str(j)+".hits",skiprows=1,names=["evid","detid","x","y","z","dphi","dtheta"], sep='\s+')
+        #print(temp.size)
+        df = pd.concat([df,temp],axis=0)
+
+    #counting the coincidence events
+    dupli = df[df.duplicated(subset=["evid"],keep=False)]
+    #print(dupli.head(10))
+    group = dupli.groupby(by=["detid"]).indices
+
+    hits = len(group[0])
+    print(f"Coincidence hits: {hits}")
+    ratio = hits/pn
+    geofac = np.pi*area*ratio
+
+    print(f"Full Geometry factor: {np.round(geofac,2)}")
+    print()
+
+    #GF of a cut out area of two 10x10 PDs as validation
+    cut = df[df["x"].between(-5,5) & df["y"].between(-5,5)]
+    dublicut = cut[cut.duplicated(subset=["evid"],keep=False)]
+
+    #print(f"cutsize: {cut.size}")
+    #print(dublicut.head(10))
+
+    cutgroup = dublicut.groupby(by=["detid"]).indices
+    #print(cutgroup)
+    hits = len(cutgroup[0])
+    print(f"10x10 hits: {hits}")
+    ratio = hits/pn
+    geofac = np.pi*area*ratio
+
+    print(f"10x10 cut GF: {np.round(geofac,5)}")
+    print()
